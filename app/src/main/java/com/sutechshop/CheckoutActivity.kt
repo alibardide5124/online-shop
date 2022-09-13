107c34cb83b2b3bed73bd3b24e05a47d403ce11d
Ali Bardide <alibardide5124@gmail.com>
2022-09-13 13:24:06 +0430

    Design some of layouts (UserPanel, Checkout, Login)

---
+++ app/src/main/java/com/sutechshop/CheckoutActivity.kt
@@ -0,0 +1,11 @@
+package com.sutechshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiry_checkout)
+    }
+}
---
+++ app/src/main/java/com/sutechshop/UserPanelActivity.kt
@@ -0,0 +1,11 @@
+package com.sutechshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserPanelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_panel)
+    }
+}
